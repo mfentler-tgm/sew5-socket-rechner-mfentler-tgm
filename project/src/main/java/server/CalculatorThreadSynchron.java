688b73a18f2f3232724af17ff35e345838e3f63b
Mario Fentler <mfentler@student.tgm.ac.at>
2019-05-04 12:51:11 +0200

    Changed repo

--- project/src/main/java/server/CalculatorThreadSynchron.java
+++ project/src/main/java/server/CalculatorThreadSynchron.java
@@ -5,10 +5,20 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.security.NoSuchAlgorithmException;
+import javax.crypto.*;
 
 public class CalculatorThreadSynchron extends CalculatorDecorator {
+    private Cipher cipher;
     public CalculatorThreadSynchron(Calculator c) {
         super(c);
+        try {
+            cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (NoSuchPaddingException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
